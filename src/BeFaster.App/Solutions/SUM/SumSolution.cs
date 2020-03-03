39754ccbbc95ac09804f072a0ab739add3d985fa
sbx_user1051 <sbx_user1051@169.254.11.93>
2020-03-03 22:21:11 +0000

    Tue Mar 03 22:21:11 UTC 2020

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -6,7 +6,15 @@ namespace BeFaster.App.Solutions.SUM
     {
         public static int Sum(int x, int y)
         {
-            throw new SolutionNotImplementedException();
+            return IsValidNumber(x) && IsValidNumber(y)
+                ? x + y
+                : -1;
+        }
+
+        private static bool IsValidNumber(int number)
+        {
+            return number >= 0 && number <= 100;
         }
     }
 }
+
