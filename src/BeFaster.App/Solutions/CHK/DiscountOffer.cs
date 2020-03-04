e94c68329af71e8143e7d93b23ad34e7b10cd1ce
sbx_user1051 <sbx_user1051@169.254.51.117>
2020-03-04 22:11:02 +0000

    Wed Mar 04 22:11:02 UTC 2020

--- src/BeFaster.App/Solutions/CHK/DiscountOffer.cs
+++ src/BeFaster.App/Solutions/CHK/DiscountOffer.cs
@@ -1,15 +1,29 @@
 ﻿namespace BeFaster.App.Solutions.CHK
 {
-    public class DiscountOffer : SpecialOffer
+    public class SpecialOffer
     {
+        public int Units { get; private set; }
         public int? Price { get; private set; }
+        public char FreeItem { get; private set; }
+        public OfferType Type { get; private set; }
 
-        public DiscountOffer(int units, int price)
-            : base(units)
+        private SpecialOffer()
+        { }
+
+        public SpecialOffer(int units, int price)
         {
+            Units = units;
             Price = price;
             Type = OfferType.discount;
         }
+
+        public SpecialOffer(int units, char freeItem)
+        {
+            Units = units;
+            FreeItem = freeItem;
+            Type = OfferType.freeItem;
+        }
     }
 }
+
 
--- src/BeFaster.App/Solutions/CHK/StockKeepingUnitRepository.cs
+++ src/BeFaster.App/Solutions/CHK/StockKeepingUnitRepository.cs
@@ -11,28 +11,28 @@ namespace BeFaster.App.Solutions.CHK
         {
             stockKeepingUnits = new List<StockKeepingUnit>
             {
-                new StockKeepingUnit('A', 50, new DiscountOffer(3, 130), new DiscountOffer(5, 200)),
-                new StockKeepingUnit('B', 30, new DiscountOffer(2, 45)),
+                new StockKeepingUnit('A', 50, new SpecialOffer(3, 130), new SpecialOffer(5, 200)),
+                new StockKeepingUnit('B', 30, new SpecialOffer(2, 45)),
                 new StockKeepingUnit('C', 20),
                 new StockKeepingUnit('D', 15),
-                new StockKeepingUnit('E', 40, new FreeItemOffer(2, 'B')),
-                new StockKeepingUnit('F', 10, new FreeItemOffer(3, 'F')),
+                new StockKeepingUnit('E', 40, new SpecialOffer(2, 'B')),
+                new StockKeepingUnit('F', 10, new SpecialOffer(3, 'F')),
                 new StockKeepingUnit('G', 20),
-                new StockKeepingUnit('H', 10, new DiscountOffer(5, 45), new DiscountOffer(10, 80)),
+                new StockKeepingUnit('H', 10, new SpecialOffer(5, 45), new SpecialOffer(10, 80)),
                 new StockKeepingUnit('I', 35),
                 new StockKeepingUnit('J', 60),
-                new StockKeepingUnit('K', 80, new DiscountOffer(2, 150)),
+                new StockKeepingUnit('K', 80, new SpecialOffer(2, 150)),
                 new StockKeepingUnit('L', 90),
                 new StockKeepingUnit('M', 15),
-                new StockKeepingUnit('N', 40, new FreeItemOffer(3, 'M')),
+                new StockKeepingUnit('N', 40, new SpecialOffer(3, 'M')),
                 new StockKeepingUnit('O', 10),
-                new StockKeepingUnit('P', 50, new DiscountOffer(5, 200)),
-                new StockKeepingUnit('Q', 30, new DiscountOffer(3, 80)),
-                new StockKeepingUnit('R', 50, new FreeItemOffer(3, 'Q')),
+                new StockKeepingUnit('P', 50, new SpecialOffer(5, 200)),
+                new StockKeepingUnit('Q', 30, new SpecialOffer(3, 80)),
+                new StockKeepingUnit('R', 50, new SpecialOffer(3, 'Q')),
                 new StockKeepingUnit('S', 30),
                 new StockKeepingUnit('T', 20),
-                new StockKeepingUnit('U', 40, new FreeItemOffer(4, 'U')),
-                new StockKeepingUnit('V', 50, new DiscountOffer(2, 90), new DiscountOffer(3, 130)),
+                new StockKeepingUnit('U', 40, new SpecialOffer(4, 'U')),
+                new StockKeepingUnit('V', 50, new SpecialOffer(2, 90), new SpecialOffer(3, 130)),
                 new StockKeepingUnit('W', 20),
                 new StockKeepingUnit('X', 90),
                 new StockKeepingUnit('Y', 10),
@@ -46,6 +46,7 @@ namespace BeFaster.App.Solutions.CHK
         }
     }
 }
+
 
 
 
