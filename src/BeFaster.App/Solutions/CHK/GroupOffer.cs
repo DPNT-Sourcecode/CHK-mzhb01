1d866307b5d58199bdd90fd131d57d8ca9f1b78a
sbx_user1051 <sbx_user1051@169.254.51.117>
2020-03-04 21:59:02 +0000

    Wed Mar 04 21:59:02 UTC 2020

--- src/BeFaster.App/Solutions/CHK/GroupOffer.cs
+++ src/BeFaster.App/Solutions/CHK/GroupOffer.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeFaster.App.Solutions.CHK
+﻿namespace BeFaster.App.Solutions.CHK
 {
-    class GroupOffer
+    public class GroupOffer
     {
+
     }
 }
+
